1c3f335fbe84f2ac22eb350101254eecb372d974
katie stout <katiestout@katies-MacBook-Pro.local>
2018-08-19 22:01:59 -0400

    arthur

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+// Bring in profile model
+const Profile = require('../../models/Profile');
+// Bring in user profile
+const Profile = require('../../models/User');
 
 //Route: GET request to api/profile/test
 //this is a public route
@@ -7,5 +14,14 @@ const router = express.Router();
 router.get('/test', (req ,res) => res.json({msg: "profile WORKS!"}));
 
 // Route that gets the current users profile
+//private access route
+//get api/profile
+
+router.get('/', passport.authenticate('jwt', {session: false}), (req, res) => {
+    Profile.findOne({ user })
+});
+
+
+
 
 module.exports = router;
