adc62f211b01583a30c45846baa33d5029c75af2
katherinestout <kjs14@hood.edu>
2018-09-20 19:21:41 -0400

    logout functionality works

--- my-app/src/actions/authActions.js
+++ my-app/src/actions/authActions.js
@@ -1,5 +1,4 @@
 import axios from 'axios';
-
 import setAuthToken from '../utils/setAuthToken';
 import jwt_decode from 'jwt-decode';
 
--- my-app/src/components/layout/Landing.js
+++ my-app/src/components/layout/Landing.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import {PropTypes} from 'prop-types';
+import {connect} from 'react-redux';
 
-export default class Landing extends Component {
+ class Landing extends Component {
   render() {
     return (
       <div className="landing ">
@@ -28,3 +30,13 @@ export default class Landing extends Component {
   };
 };
 
+//add proptypes
+Landing.propTypes = {
+  auth: PropTypes.object.isRequired
+}
+
+const mapStateToProps = (state) => ({
+auth: state.auth
+});
+
+export default connect(mapStateToProps)(Landing);
