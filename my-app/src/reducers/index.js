cb16bf7741c34d038bb760508ac04dff52f11cac
katherinestout <kjs14@hood.edu>
2018-09-27 22:48:38 -0400

    profilereducer

--- my-app/src/reducers/index.js
+++ my-app/src/reducers/index.js
@@ -1,9 +1,21 @@
 import { combineReducers } from 'redux';
 import authReducer from './authReducer';
 import errorReducer from './errorReducer';
+import profileReducer from './profileReducer';
+
+
+//all of the reducers
+/*
+a reducer is a function that takes the current state and an action, 
+and returns the next state. 
+They specify how the applications state changes in response to actions
+sent to the store.
+In essence, simplifies things and "reduces" them
+*/
 
 
 export default combineReducers ({
     auth: authReducer,
-    errors: errorReducer
+    errors: errorReducer,
+    profile: profileReducer
 });
---
+++ my-app/src/reducers/profileReducer.js
@@ -0,0 +1,16 @@
+//fetching profiles
+
+
+const initialState = {
+    profile: null,
+    profiles: null,
+    loading: false
+}
+
+export default function( state = initialState, action){
+    //switch statement based on type
+switch(action.type) {
+    default:
+    return state;
+}
+}
