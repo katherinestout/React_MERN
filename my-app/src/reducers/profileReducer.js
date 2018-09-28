8d354563130ab17f29392f6e0b75621698290cd2
katherinestout <kjs14@hood.edu>
2018-09-27 23:07:21 -0400

    profile reducer

--- my-app/src/reducers/profileReducer.js
+++ my-app/src/reducers/profileReducer.js
@@ -1,6 +1,8 @@
+import {GET_PROFILE, PROFILE_LOADING} from '../actions/types';
+
+
+
 //fetching profiles
-
-
 const initialState = {
     profile: null,
     profiles: null,
@@ -10,6 +12,21 @@ const initialState = {
 export default function( state = initialState, action){
     //switch statement based on type
 switch(action.type) {
+    //profile_loading, setstate
+    case PROFILE_LOADING:
+    return{
+        ...state,
+        loading: true
+    };
+    //Get_profile
+    //payload will be empty and get set to that user
+    //if there is no profile yet, will be able to create a profile
+    case GET_PROFILE:
+    return{
+        ...state,
+        profile: action.payload,
+        loading: false
+    };
     default:
     return state;
 }
