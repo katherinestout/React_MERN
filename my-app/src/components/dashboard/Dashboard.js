21fc47bbb78bc38d704b17c95f2aa175cfbbd439
katherinestout <kjs14@hood.edu>
2018-09-28 12:36:53 -0400

    dashboard is set up for no profile

--- my-app/src/components/dashboard/Dashboard.js
+++ my-app/src/components/dashboard/Dashboard.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import {getCurrentProfile} from '../../actions/profileActions';
@@ -18,11 +19,29 @@ class Dashboard extends Component{
         //want to make sure profile state is not null
         //if profile is equal to null or loading is true
         if(profile === null || loading) {
-            //if it is null
+            //if it is null:
             dashboardContent = <h4>Loading...</h4>
 
         } else {
-            dashboardContent = <h1>Hello!</h1>
+    //Check if logged in user has profile data
+       //object.keys gets key of profile object
+       //if it is greater than 0 than that means there is already a profile created
+           if(Object.keys(profile).length > 0){
+                dashboardContent = <h4>ya gotta DISPLAY PROFILE</h4>
+           } else {
+        //user is logged in but has no profile
+        dashboardContent = (
+            <div>
+                <p className="lead text-muted">
+                Welcome {user.name}
+                </p>
+                <p>Ya gotta set up a profile so add some info</p>
+                <Link to ="/create-profile" className="btn btn-dark">
+                Create a Profile!
+                </Link>
+            </div>
+        )
+           }
         }
         return(
 <div className="dashboard">
@@ -31,6 +50,7 @@ class Dashboard extends Component{
 <div className ="col-md-12">
 
  <h1 className="display-4">Dashboard</h1>
+ 
 {dashboardContent}
 </div>
 </div>
