d6fd105c279950bce3ab79beb1c487b6f8e37b2c
katherinestout <kjs14@hood.edu>
2018-12-13 16:36:21 -0500

    creating profile

--- my-app/src/components/create-profile/CreateProfile.js
+++ my-app/src/components/create-profile/CreateProfile.js
@@ -11,18 +11,58 @@ export class CreateProfile extends Component {
     constructor(props){
         super(props);
         this.state = {
-            displaySocialInputs: false,
+            
             handle: '',
-            twitter: '',
             bio: '',
+            twitter: '',
             instagram: '',
             errors: {}
 
-        }
+        };
+        this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+    onSubmit(e){
+        e.preventDefault();
+        console.log('submit');
+    }
+    onChange(e){
+        this.setState({[e.target.name]: e.target.value});
     }
 
 
   render() {
+      const {errors, displaySocialInputs} =this.state;
+
+      let socialInputs;
+
+      if(displaySocialInputs){
+        socialInputs=(
+            <div>
+                <InputGroup
+                placeholder="Twitter Profile URL"
+                name="twitter"
+                icon="fab fa-twitter"
+                value ={this.state.twitter}
+                onChange={this.onChange}
+                error={errors.twitter}
+                />
+
+                <InputGroup
+                placeholder="Instagram Profile URL"
+                name="instagram"
+                icon="fab fa-instagram"
+                value ={this.state.instagram}
+                onChange={this.onChange}
+                error={errors.instagram}
+                />
+
+        
+            </div>
+        )
+      } 
+
+    
     return (
       <div className="create-profile">
         <div className ="container">
@@ -35,13 +75,44 @@ export class CreateProfile extends Component {
         </p>
         <small className="d-block pb-3"> * = required fields
         </small>
-        
+        <form onSubmit={this.onSubmit}>
+        <TextFieldGroup
+        placeholder="* Profile Handle"
+        name="handle"
+        value={this.state.handle}
+        onChange={this.onChange}
+        error={errors.handle}
+        info="A unique handle for your profile URL. Your full name, nickname ect."
+        />
+          <TextAreaFieldGroup
+        placeholder="* Short Bio"
+        name="bio"
+        value={this.state.bio}
+        onChange={this.onChange}
+        error={errors.bio}
+        info="A little about yourself"
+        />
+        <div className="mb-3">
+        <button onClick = {() => {
+            this.setState(prevState => ({
+                displaySocialInputs: !prevState.displaySocialInputs
+            }))
+        }} className="btn btn-light">
+        Add Social Network Links
+         </button>
+         <span className="text-muted">Optional</span>
+         </div>
+
+
+        {socialInputs}
+        <input type = "submit"
+            value="submit"
+            className="btn btn-info btn-block mt-4"/>
+
+        </form>
         </div> 
-        
         </div>
-        
         </div>
-        
       </div>
     )
   }
