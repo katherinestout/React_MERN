85dea598e0344a6e09684ebb9c62628579fdf7de
katherinestout <kjs14@hood.edu>
2019-01-30 13:59:33 -0500

    keys dev

---
+++ config/keys_dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: 'mongodb://Katie123:Katie123@cluster0-shard-00-00-o24k5.mongodb.net:27017,cluster0-shard-00-01-o24k5.mongodb.net:27017,cluster0-shard-00-02-o24k5.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true',
+    secretOrKey: 'secret'
+};
